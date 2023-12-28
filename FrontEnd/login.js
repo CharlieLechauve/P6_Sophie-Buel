a72d01c70cf65326086faacb853ab7266331271a
Charlie LECHAUVE <lechauve.charlie@orange.fr>
2023-12-28 16:54:34 +0100

    login done

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,90 +1,47 @@
-// DOMContentLoaded permet d'attendre la connexion de la page avant le lancement du script
 document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("login__form");
+    const errorText = document.getElementById("login__error");
+    const mailInput = document.getElementById("email");
+    const pwInput = document.getElementById("password");
 
-const alreadyLoggedError = document.querySelector(".alreadyLogged__error"); 
-const loginEmailError = document.querySelector(".loginEmail__error"); 
-const loginMdpError = document.querySelector(".loginMdp__error"); 
-
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-
-const submit = document.getElementById("submit");
-
-alreadyLogged();
-
-// Utilisateur déjà connecté : Suppression token
-function alreadyLogged() {
-    if (localStorage.getItem("token")) {
-        localStorage.removeItem("token");
-
-        const p = document.createElement("p");
-        p.innerHTML = "Vous avez été déconnecté";
-        alreadyLoggedError.appendChild(p);
-        return;
-    }
-}
-
-// Event Listener connexion
-submit.addEventListener("click", () => {
-    let user = {
-        email: email.value,
-        password: password.value
-    };
-    login(user);
-})
-
-// Fonction Connexion
-function login(id) {
-    console.log(id);
-    loginEmailError.innerHTML = "";
-    loginMdpError.innerHTML = "";
-
-
-    // Vérification E-mail
-    if (!id.email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/g)) {
-        const p = document.createElement("p");
-        p.innerHTML = "Adresse e-mail invalide";
-        loginEmailError.appendChild(p);
-        return;
+    function clearErrorMessage() {
+        errorText.innerHTML = "";
     }
 
-    // Vérification Password
-    if (id.password.length < 5 && !id.password.match(/^[a-zA-Z0-9]+$/g)) {
-        const p = document.createElement("p");
-        p.innerHTML = "Mot de passe invalide";
-        loginMdpError.appendChild(p);
-        return;
+    function setErrorMessage(Error) {
+        errorText.innerHTML = Error;
     }
 
-    // Vérification de l'email et du mot de passe
-    else {
-    fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(id)
-    })
-    .then(response => response.json())
-    .then(result => { 
-        console.log(result);
-        
-        //  Connexion impossible
-        if (result.error || result.message) {
-            const p = document.createElement("p");
-            p.innerHTML = "Ce mot de passe était incorrect. Veuillez réessayer.";
-            loginMdpError.appendChild(p);
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-        // Connexion possible
-        } else if (result.token) {
-            localStorage.setItem("token", result.token);
-            window.location.href = "index.html";
+        const mail = mailInput.value.trim(); // Utilisez la valeur réelle du champ email
+        const pw = pwInput.value.trim();
+
+        try {
+            let apiResponse = await fetch("http://localhost:5678/api/users/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: mail, password: pw }), // Utilisez les variables mail et pw
+            });
+
+            if (apiResponse.status === 200) {
+                const data = await apiResponse.json();
+                sessionStorage.setItem("token", data.token);
+                window.location.href = "index.html";
+            } else if (apiResponse.status === 401) {
+                setErrorMessage('Adresse ou mot de passe incorrect');
+            } else if (apiResponse.status === 404) {
+                setErrorMessage('Adresse ou mot de passe incorrect');
+            } else {
+                const errorMessage = await apiResponse.text();
+                setErrorMessage(errorMessage || 'Une erreur est survenue');
+            }
+        } catch (error) {
+            console.error(error);
+            setErrorMessage('Une erreur est survenue');
         }
-    
-    })
-    
-    // Envoie du message d'erreur
-    .catch(error => 
-        console.log(error));
-}}
+    });
 });
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,101 +1,94 @@
-//Constantes FILTRE
-const btnAll = document.querySelector(".filter__btn-id-null");
-const btnId1 = document.querySelector(".filter__btn-id-1");
-const btnId2 = document.querySelector(".filter__btn-id-2");
-const btnId3 = document.querySelector(".filter__btn-id-3");
+const gallery = document.querySelector('.gallery');
+const filtersContainer = document.querySelector('.filters');
 
-//Constante Projet
-const sectionProjets = document.querySelector(".gallery");
-
-let data = null;
-let id;
-
-function main() {
-    generationProjets(data, null);
+async function main() {
+    displayWorks();  // Affiche tous les projets initialement
+    displayFilters();  // Affiche les filtres
 }
 
-main()
+main();
 
-// Fonction reset Projets
-function resetSectionProjets() {  
-	sectionProjets.innerHTML = "";
-}
-
-//-----------------------------------//
-//--Fonction génération des Projets--//
-//-----------------------------------//
-async function generationProjets(data, id) { 
-
-    //Vérifier si Fetch a été lancé
+async function getWorks() {
     try {
-        const response = await fetch('http://localhost:5678/api/works');
-        data = await response.json();
+        const worksResponse = await fetch('http://localhost:5678/api/works');
+        return worksResponse.json();
+    } catch (error) {
+        console.log("Erreur lors de la récupération des projets depuis l'API");
     }
+}
 
-    //Si Fetch n'a pas été lancé
-    catch{
-        const p = document.createElement("p");
-        p.classList.add("error");
-        p.innerHTML = "Une erreur est survenue lors de la récupération des projets";
-        sectionProjets.appendChild(p);
-        await new Promise(resolve => setTimeout(resolve, 60000)); //(Temps en milisecondes)
-        window.location.href = "index.html";
+async function getCategories() {
+    try {
+        const categoriesResponse = await fetch('http://localhost:5678/api/categories');
+        return await categoriesResponse.json();
+    } catch (error) {
+        console.log("Erreur lors de la récupération des catégories depuis l'API");
     }
+}
 
-    resetSectionProjets()
+async function displayWorks(categoryId) {
+    try {
+        const dataWorks = await getWorks();
+        gallery.innerHTML = "";
 
-    // Filtres
-    if ([1, 2, 3].includes(id)) {
-        data = data.filter(data => data.categoryId == id);}
+        // Création des projets pour affichage
+        dataWorks.map((works) => {
+            if (categoryId == works.category.id || categoryId == null) {
+                createWorks(works);
+            }
+        });
 
-    document.querySelectorAll(".filter__btn").forEach(btn => {
-        btn.classList.remove("filter__btn--active");})
-    document.querySelector(`.filter__btn-id-${id}`).classList.add("filter__btn--active");
+    } catch (error) {
+        console.log("Erreur lors de l'affichage des projets");
+    }
+}
 
-    if (data.length === 0 || data === undefined) { 
-        const p = document.createElement("p");
-        p.classList.add("error");
-        p.innerHTML = "Aucun projet à afficher";
-        sectionProjets.appendChild(p);
-        return;}
+// Fonction création d'un projet dans le DOM
+function createWorks(works) {
+    //Création des éléments "fictivement"
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
 
-    // Génère les projets
-    if (id === null || [1, 2, 3].includes(id)) {
-        for (let i = 0; i < data.length; i++) {
-            
-            const figure = document.createElement("figure"); 
-            sectionProjets.appendChild(figure);
-            figure.classList.add(`js-projet-${data[i].id}`); // Ajoute l'id du projet 
-            const img = document.createElement("img");
-            img.src = data[i].imageUrl;
-            img.alt = data[i].title;
-            figure.appendChild(img);
+    //Liens où récupérer img, figcaption et catégorie
+    img.src = works.imageUrl;
+    figcaption.innerText = works.title;
+    figure.setAttribute("categoryId", works.category.id);
 
-            const figcaption = document.createElement("figcaption");
-            figcaption.innerHTML = data[i].title;
-            figure.appendChild(figcaption);
-        }
-}}
+    //Placement dans DOM
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+}
 
-//--------------------//
-//--Filtres Listener--//
-//--------------------//
-btnAll.addEventListener("click", () => {
-    generationProjets(data, null);})
+// Fonction filtre associé aux boutons (créés dans la fonction directement)
+async function displayFilters() {
+    try {
+        const dataCategories = await getCategories();
 
-btnId1.addEventListener("click", () => {
-    generationProjets(data, 1);})
+        // Création des boutons
+        dataCategories.map((category) => {
+            const btnCategory = document.createElement("button");
+            btnCategory.innerText = category.name;
+            btnCategory.setAttribute("class", "filter__btn");
+            btnCategory.setAttribute("id", category.id);
+            filtersContainer.appendChild(btnCategory);
+        });
 
-btnId2.addEventListener("click", () => {
-    generationProjets(data, 2);})
+        // EventListeners sur les boutons
+        const buttons = document.querySelectorAll(".filters button");
 
-btnId3.addEventListener("click", () => {
-    generationProjets(data, 3);})
-
-
-
-//------------------------------//
-//------------------------------//
-//------------MODALE------------//
-//------------------------------//
-//------------------------------//
+        //Fonction appliquée à chaques boutons
+        buttons.forEach((button) => {
+        
+            button.addEventListener("click", () => {
+                const categoryId = button.getAttribute("id");
+                buttons.forEach((button) => button.classList.remove("filter__btn--active"));
+                button.classList.add("filter__btn--active");
+                    displayWorks(categoryId);
+            });
+        });
+    } catch (error) {
+        console.log("Erreur lors de la création des boutons");
+    }
+}
